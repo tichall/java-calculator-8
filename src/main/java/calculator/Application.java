9bf03bd04faed74960995870dcda29432280f8c2
tichall <0817frog@gmail.com>
2025-10-20 21:49:24 +0900

    feat(Application): 커스텀 구분자 추출 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,22 +1,35 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     private static final List<String> defaultDelimiters = List.of(",", ":");
-    private static final String delimiterRegex = String.join("|", defaultDelimiters);
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+    private static String customDelimiter = "";
+    private static String delimiterRegex = String.join("|", defaultDelimiters);
 
     public static void main(String[] args) {
-        start();
+        run();
     }
 
-    public static void start() {
+    public static void run() {
         String input = readInput();
         int sum = 0;
+        String targetString = input;
+        List<String> numberStrings = new ArrayList<>();
 
-        if(isNotBlank(input)) {
-            String[] numberStrings = input.split(delimiterRegex);
+        if (isNotBlank(input)) {
+            if (existsCustomDelimiter(input)) {
+                targetString = input.substring(
+                        input.indexOf(CUSTOM_DELIMITER_SUFFIX) + CUSTOM_DELIMITER_SUFFIX.length());
+                delimiterRegex = customDelimiter;
+            }
+
+            numberStrings.addAll(Arrays.asList(targetString.split(delimiterRegex)));
 
             for (String numberString : numberStrings) {
                 try {
@@ -44,5 +57,29 @@ public class Application {
 
     public static boolean isNotBlank(String input) {
         return !input.trim().isEmpty();
+    }
+
+    public static boolean existsCustomDelimiter(String input) {
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            int lastIndexOfCustomDelimiter = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+
+            if (lastIndexOfCustomDelimiter == -1) {
+                throw new IllegalArgumentException("커스텀 구분자 지정이 유효하지 않습니다.");
+            }
+
+            customDelimiter = input.substring(CUSTOM_DELIMITER_PREFIX.length(), lastIndexOfCustomDelimiter);
+            validateDelimiterIsNotNumeric(customDelimiter);
+            return true;
+        }
+        return false;
+    }
+
+    public static void validateDelimiterIsNotNumeric(String delimiter) {
+        try {
+            Double.parseDouble(delimiter);
+            throw new IllegalArgumentException("커스텀 지정자로 숫자를 사용할 수 없습니다.");
+        } catch (NumberFormatException ignore) {
+
+        }
     }
 }
